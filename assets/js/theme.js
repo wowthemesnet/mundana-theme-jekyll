7f249906fb4896eae8d48d21cc0ba2d859ee06c2
Reinier van der Leer <reintjebadboy@gmail.com>
2019-10-07 00:57:45 +0200

    theme.js: fixed and updated syntax for ECMAScript 6 compliance, optimized JQuery selectors;

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,6 @@
  // alertbar later
     $(document).scroll(function () {
-        var y = $(this).scrollTop();
+        const y = $(this).scrollTop();
         if (y > 280) {
             $('.alertbar').fadeIn();
         } else {
@@ -10,12 +10,13 @@
 
 
 // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('nav').outerHeight();
+    let didScroll;
+    let lastScrollTop = 0;
+    const delta = 5;
+    const $nav = $('nav');
+    const navbarHeight = $nav.outerHeight();
 
-    $(window).scroll(function(event){
+    $(window).scroll(function(){
         didScroll = true;
     });
 
@@ -27,7 +28,7 @@
     }, 250);
 
     function hasScrolled() {
-        var st = $(this).scrollTop();
+        const st = $(this).scrollTop();
         
         // Make sure they scroll more than delta
         if(Math.abs(lastScrollTop - st) <= delta)
@@ -37,13 +38,13 @@
         // This is necessary so you never see what is "behind" the navbar.
         if (st > lastScrollTop && st > navbarHeight){
             // Scroll Down            
-            $('nav').removeClass('nav-down').addClass('nav-up'); 
-            $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
+            $nav.removeClass('nav-down').addClass('nav-up');
+            $('.nav-up').css('top', - $nav.outerHeight() + 'px');
            
         } else {
             // Scroll Up
             if(st + $(window).height() < $(document).height()) {               
-                $('nav').removeClass('nav-up').addClass('nav-down');
+                $nav.removeClass('nav-up').addClass('nav-down');
                 $('.nav-up, .nav-down').css('top', '0px');             
             }
         }
@@ -58,49 +59,49 @@
 function loadSearch(){
     // Create a new Index
     idx = lunr(function(){
-        this.field('id')
-        this.field('title', { boost: 10 })
-        this.field('summary')
-    })
+        this.field('id');
+        this.field('title', { boost: 10 });
+        this.field('summary');
+    });
  
     // Send a request to get the content json file
     $.getJSON('/content.json', function(data){
  
         // Put the data into the window global so it can be used later
-        window.searchData = data
+        window.searchData = data;
  
         // Loop through each entry and add it to the index
         $.each(data, function(index, entry){
-            idx.add($.extend({"id": index}, entry))
-        })
-    })
+            idx.add($.extend({"id": index}, entry));
+        });
+    });
  
     // When search is pressed on the menu toggle the search box
     $('#search').on('click', function(){
-        $('.searchForm').toggleClass('show')
-    })
+        $('.searchForm').toggleClass('show');
+    });
  
     // When the search form is submitted
     $('#searchForm').on('submit', function(e){
         // Stop the default action
-        e.preventDefault()
+        e.preventDefault();
  
         // Find the results from lunr
-        results = idx.search($('#searchField').val())
+        let results = idx.search($('#searchField').val());
  
         // Empty #content and put a list in for the results
         $('#content').html('<h1>Search Results (' + results.length + ')</h1>')
-        $('#content').append('<ul id="searchResults"></ul>')
+                     .append('<ul id="searchResults"></ul>');
  
         // Loop through results
         $.each(results, function(index, result){
             // Get the entry from the window global
-            entry = window.searchData[result.ref]
+            let entry = window.searchData[result.ref];
  
             // Append the entry to the list.
-            $('#searchResults').append('<li><a href="' + entry.url + '">' + entry.title + '</li>')
-        })
-    })
+            $('#searchResults').append('<li><a href="' + entry.url + '">' + entry.title + '</li>');
+        });
+    });
 }
 
 
@@ -111,7 +112,7 @@ $(function() {
     if (location.hash) {
       /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
       window.scrollTo(0, 0);
-      target = location.hash.split('#');
+      let target = location.hash.split('#');
       smoothScrollTo($('#'+target[1]));
     }
   }, 1);

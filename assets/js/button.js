100c4a59b96ae8f23a6a228453411a218e42eac5
AdamBougher <a.bougher1999@gmail.com>
2024-02-16 15:02:35 -0600

    Started Working on a Carosell/gallery

---
+++ assets/js/button.js
@@ -0,0 +1,14 @@
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= itemWidth + padding
+})
+
+next.addEventListener('click',()=>{
+  list.scrollLeft += itemWidth + padding
+})
